9fdc39b6e11cebc849ce10f323c09b9bc79f327e
Prosenjit Manna <prosenjit@itobuz.com>
2023-07-23 17:50:34 +0530

    fix: unused code cleanup

--- src/Components/Header/HeaderComponent.tsx
+++ src/Components/Header/HeaderComponent.tsx
@@ -81,30 +81,6 @@ export default function HeaderComponent({
             </div>
           </div>
         </div>
-        <div className='md:hidden'>
-          <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-            <a
-              className='text-gray-600 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-              href='/#'>
-              Home
-            </a>
-            <a
-              className='text-gray-800 dark:text-white block px-3 py-2 rounded-md text-base font-medium'
-              href='/#'>
-              Gallery
-            </a>
-            <a
-              className='text-gray-600 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-              href='/#'>
-              Content
-            </a>
-            <a
-              className='text-gray-600 hover:text-gray-800 dark:hover:text-white block px-3 py-2 rounded-md text-base font-medium'
-              href='/#'>
-              Contact
-            </a>
-          </div>
-        </div>
       </nav>
     </div>
   );
