f3e153e1d189b62ab343f10b4218e7ca3d904a0d
Prosenjit Manna <prosenjit@itobuz.com>
2023-07-23 17:11:03 +0530

    feat: remove popup and integrate with menu

--- src/Components/Header/HeaderComponent.tsx
+++ src/Components/Header/HeaderComponent.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable max-len */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Popup from 'reactjs-popup';
 import routes from '../../Lib/Routes/Routes';
 import { useAppDispatch } from '../../Lib/Store/hooks';
 import { userSliceActions } from '../../Lib/Store/User/User.Slice';
 import { useViewportSize } from '@mantine/hooks';
-import { Burger } from '@mantine/core';
+import { Burger, Button, Menu } from '@mantine/core';
+import { FaUserLarge } from 'react-icons/fa6';
 
 export default function HeaderComponent({
   handleMobileDrawer,
@@ -56,57 +56,19 @@ export default function HeaderComponent({
             {width > 767 && (
               <div className='block'>
                 <div className='ml-4 flex items-center md:ml-6'>
-                  <div className='ml-3 relative'>
-                    <div className='relative inline-block text-left'>
-                      <Popup
-                        trigger={
-                          <div>
-                            <button
-                              type='button'
-                              className='  flex items-center justify-center w-full rounded-md  px-4 py-2 text-sm font-medium text-gray-700  hover:bg-gray-50  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-gray-500'
-                              id='options-menu'>
-                              <svg
-                                width='20'
-                                fill='currentColor'
-                                height='20'
-                                className='text-gray-800'
-                                viewBox='0 0 1792 1792'
-                                xmlns='http://www.w3.org/2000/svg'>
-                                <path d='M1523 1339q-22-155-87.5-257.5t-184.5-118.5q-67 74-159.5 115.5t-195.5 41.5-195.5-41.5-159.5-115.5q-119 16-184.5 118.5t-87.5 257.5q106 150 271 237.5t356 87.5 356-87.5 271-237.5zm-243-699q0-159-112.5-271.5t-271.5-112.5-271.5 112.5-112.5 271.5 112.5 271.5 271.5 112.5 271.5-112.5 112.5-271.5zm512 256q0 182-71 347.5t-190.5 286-285.5 191.5-349 71q-182 0-348-71t-286-191-191-286-71-348 71-348 191-286 286-191 348-71 348 71 286 191 191 286 71 348z'></path>
-                              </svg>
-                            </button>
-                          </div>
-                        }
-                        position='bottom center'
-                        on='hover'
-                        closeOnDocumentClick
-                        mouseLeaveDelay={300}
-                        mouseEnterDelay={0}
-                        contentStyle={{ padding: '0px', border: 'none' }}>
-                        <div className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white  ring-1 ring-black ring-opacity-5'>
-                          <div
-                            className='py-1 '
-                            role='menu'
-                            aria-orientation='vertical'
-                            aria-labelledby='options-menu'>
-                            <Link
-                              to={routes.dashboard.children.me.path}
-                              className='block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 outline-none'
-                              role='menuitem'>
-                              <span className='flex flex-col'>
-                                <span>Account</span>
-                              </span>
-                            </Link>
-                            <button
-                              onClick={logOut}
-                              className='block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 w-full text-left'>
-                              Logout
-                            </button>
-                          </div>
-                        </div>
-                      </Popup>
-                    </div>
-                  </div>
+                  <Menu
+                    shadow='md'
+                    width={200}>
+                    <Menu.Target>
+                      <Button>
+                        <FaUserLarge />
+                      </Button>
+                    </Menu.Target>
+
+                    <Menu.Dropdown>
+                      <Menu.Item onClick={logOut}>Logout</Menu.Item>
+                    </Menu.Dropdown>
+                  </Menu>
                 </div>
               </div>
             )}
@@ -116,7 +78,6 @@ export default function HeaderComponent({
                 opened={sidebarOpened || false}
                 onClick={handleMobileDrawer}
               />
-              
             </div>
           </div>
         </div>
--- src/Page/Dashboard/Post/PostList/PostList.tsx
+++ src/Page/Dashboard/Post/PostList/PostList.tsx
@@ -2,15 +2,13 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
 import queryString from 'query-string';
 
-import ButtonComponent, { defaultButtonConfig } from 'Components/Button/ButtonComponent';
 import { useAppSelector } from 'Lib/Store/hooks';
 import { postSliceActions } from 'Lib/Store/Post/Post.slice';
 import { PostListRouteQuery } from 'Lib/Routes/routes.interface';
 import { postRoutes } from 'Lib/Routes/PostRoutes';
+import { Button, Menu } from '@mantine/core';
 
 export default function PostListPage() {
   const dispatch = useDispatch();
@@ -18,18 +16,15 @@ export default function PostListPage() {
   const [page, setPage] = useState(1);
   const location = useLocation();
 
-
   useEffect(() => {
     const q: PostListRouteQuery = queryString.parse(location.search);
-    console.log('post-list query', q);
-    
+
     if (q.page) {
       setPage(+q.page);
       dispatch(postSliceActions.getPosts({ _page: +q.page, _limit: 10 }));
     } else {
       dispatch(postSliceActions.getPosts({ _limit: 10 }));
     }
-
   }, [location.search]);
 
   return (
@@ -42,7 +37,9 @@ export default function PostListPage() {
               style={{ width: '100px' }}>
               ID
             </th>
-            <th className='border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900'>Title</th>
+            <th className='border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900'>
+              Title
+            </th>
             <th
               className='border-b-2 p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900'
               style={{ width: '100px' }}>
@@ -53,7 +50,9 @@ export default function PostListPage() {
         <tbody>
           {postLists.map((post) => {
             return (
-              <tr className='text-gray-700' key={post.id}>
+              <tr
+                className='text-gray-700'
+                key={post.id}>
                 <td className='border-b-2 p-4 dark:border-dark-5'>{post.id}</td>
                 <td className='border-b-2 p-4 dark:border-dark-5'>
                   <Link
@@ -63,13 +62,20 @@ export default function PostListPage() {
                   </Link>
                 </td>
                 <td className='border-b-2 p-4 dark:border-dark-5'>
-                  <Popup trigger={<button>...</button>} position='bottom right'>
-                    <div className='p-2'>
+                  <Menu
+                    shadow='md'
+                    position='right-start'
+                    width={200}>
+                    <Menu.Target>
+                      <Button variant='subtle'>...</Button>
+                    </Menu.Target>
+
+                    <Menu.Dropdown>
                       <Link to={postRoutes.post.fullPath({ postId: post.id })}>
-                        <ButtonComponent color={defaultButtonConfig.color} >Edit</ButtonComponent>
+                        <Menu.Item>Edit</Menu.Item>
                       </Link>
-                    </div>
-                  </Popup>
+                    </Menu.Dropdown>
+                  </Menu>
                 </td>
               </tr>
             );
@@ -78,26 +84,28 @@ export default function PostListPage() {
       </table>
       <div className='px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between'>
         <div className='flex items-center'>
-        <Link to={postRoutes.list.build({ page: (page + -1) })}>
-          <button
-            type='button'
-            className='w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100'>
-            <svg
-              width='9'
-              fill='currentColor'
-              height='8'
-              className=''
-              viewBox='0 0 1792 1792'
-              xmlns='http://www.w3.org/2000/svg'>
-              <path d='M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z'></path>
-            </svg>
-          </button>
+          <Link to={postRoutes.list.build({ page: page + -1 })}>
+            <button
+              type='button'
+              className='w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100'>
+              <svg
+                width='9'
+                fill='currentColor'
+                height='8'
+                className=''
+                viewBox='0 0 1792 1792'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path d='M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z'></path>
+              </svg>
+            </button>
           </Link>
-          <div style={{ minWidth: '100px' }} className="text-center">
+          <div
+            style={{ minWidth: '100px' }}
+            className='text-center'>
             Page: {page}
           </div>
-          
-          <Link to={postRoutes.list.build({ page: (page + 1) })}>
+
+          <Link to={postRoutes.list.build({ page: page + 1 })}>
             <button
               type='button'
               className='w-full p-4 border-t border-b border-r text-base  rounded-r-xl text-gray-600 bg-white hover:bg-gray-100'>
