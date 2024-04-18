44cad837fe477983dee4198f49ffb3eec27e86d8
Prosenjit Manna <prosenjit@itobuz.com>
2024-04-18 12:52:44 +0530

    feat: commit lint and commitzen  PROJ-1

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,14 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ['PROJ-']
+    }
+  },
+  rules: {
+    'subject-case': [0],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'references-empty': [2, 'never']
+  }
+};
