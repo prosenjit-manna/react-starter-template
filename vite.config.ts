b20d85ee0cd05a056779ec759a4a6439c7575163
Prosenjit Manna <prosenjit@itobuz.com>
2024-04-22 19:34:17 +0530

    feat: pwa added

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import eslint from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: '',
-  plugins: [react(), viteTsconfigPaths(), eslint()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    eslint(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{tsx,ts,js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: false,
+      },
+    }),
+  ],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
