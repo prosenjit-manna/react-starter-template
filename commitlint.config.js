2331c4785fab7d06525c9c8aa63c58551ccd0b63
Prosenjit Manna <prosenjit@itobuz.com>
2024-04-22 14:26:41 +0530

    feat: cleanup issue prefix

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,13 +2,13 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   parserPreset: {
     parserOpts: {
-      issuePrefixes: ['PROJ-']
+      issuePrefixes: ['']
     }
   },
   rules: {
     'subject-case': [0],
     'subject-empty': [2, 'never'],
     'type-empty': [2, 'never'],
-    'references-empty': [2, 'never']
+    'references-empty': [0, 'never']
   }
 };
